c5d394cf9c6ce50e36280cc8ebeb433b91642cca
prinseric007 <prinseric007@gmail.com>
2026-06-20 13:44:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -47,11 +47,7 @@ def send_movie(message):
         )
 
     elif kod == "103":
-        bot.send_vedio(
-            message.chat.id,
-            "BAACAgIAAxkBAAFM1PVqNIfoR-m4YALqeUKGuxtm0t3vaAACNoIAAiEMAAFIO6Xr-2U2ehI8BA",
-            caption="🎬 **Кино номи:** Гринландия 1\n\n🍿 Ёкимли томоша!"
-        )
+             bot.send_video(message.chat.id, "BAACAgIAAxkBAAFM1PVqNIfoR-m4YALqeUKGuxtm0t3vaAACNoIAAiEMAAFIO6Xr-2U2ehI8BA", caption="🎬 **Номи:** Гриландия 1\n\n🍿Ёкимли томоша")
         
     # Янги кино қўшмоқчи бўлсангиз, мана шу пастдаги блокни нусхалаб кўпайтираверасиз:
     # elif kod == "ЯНГИ_КОД":
